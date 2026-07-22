9f642206780ec476aa20b92ec7bf94ec6bb3c11b
Evans <everbeen1993@outlook.com>
2026-07-22 19:43:40 +0800

    Update build script

--- docker/build-image.sh
+++ docker/build-image.sh
@@ -9,17 +9,17 @@ IMAGES_DIR=$BASH_TAB_ROOT/images
 IMAGE_NAME=bashtab-v86
 CONTAINER_NAME=bashtab-v86-temp
 
+rm -r images
 
-
-# sudo env HTTP_PROXY="$HTTP_PROXY" HTTPS_PROXY="$HTTPS_PROXY" \
-#     docker build \
-#     --build-arg "HTTP_PROXY=$HTTP_PROXY" \
-#     --build-arg "HTTPS_PROXY=$HTTPS_PROXY" \
-#     --platform linux/386 \
-#     --rm \
-#     --tag "$IMAGE_NAME" \
-#     --file docker/Dockerfile \
-#     .
+sudo env HTTP_PROXY="$HTTP_PROXY" HTTPS_PROXY="$HTTPS_PROXY" \
+    docker build \
+    --build-arg "HTTP_PROXY=$HTTP_PROXY" \
+    --build-arg "HTTPS_PROXY=$HTTPS_PROXY" \
+    --platform linux/386 \
+    --rm \
+    --tag "$IMAGE_NAME" \
+    --file docker/Dockerfile \
+    .
 
 sudo docker rm "$CONTAINER_NAME" || true
 mkdir -p "$IMAGES_DIR"
